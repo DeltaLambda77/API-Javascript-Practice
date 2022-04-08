36debcdd76b363db75fd52906069d0cd0d05fdf6
danlozano77 <danlozano77.gmail.com>
2022-04-07 23:05:49 -0400

    added basic HTML + JS

---
+++ API_JS_Template/app.js
@@ -0,0 +1,4 @@
+const dogButton = document.getElementById("dog-button");
+const duckButton = document.getElementById("duck-button");
+const randomDuck = document.getElementById("random-duck");
+const randomDog = document.getElementById("random-dog");
