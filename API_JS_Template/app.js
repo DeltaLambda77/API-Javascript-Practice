8d2cceef85e0c1096478dd09ff4883a70f24f420
danlozano77 <danlozano77.gmail.com>
2022-04-08 00:13:12 -0400

    added JS functionality

--- API_JS_Template/app.js
+++ API_JS_Template/app.js
@@ -2,3 +2,19 @@ const dogButton = document.getElementById("dog-button");
 const duckButton = document.getElementById("duck-button");
 const randomDuck = document.getElementById("random-duck");
 const randomDog = document.getElementById("random-dog");
+
+dogButton.addEventListener("click", function(){
+    fetch('https://random.dog/woof.json')
+        .then(response => response.json())
+        .then(data => {
+            randomDog.innerHTML = `<img src="${data.url}" alt="A random dog image"/>`
+        }); 
+})
+
+duckButton.addEventListener("click", function(){
+    fetch('https://randomfox.ca/floof/')
+        .then(response => response.json())
+        .then(data => {
+            randomDuck.innerHTML = `<img src="${data.image}" alt="A random fox image"/>`
+        });
+})
